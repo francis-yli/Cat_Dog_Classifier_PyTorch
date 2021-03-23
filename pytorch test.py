d869db01c0732cbb4a7d01d6388aa96ca8b4611e
yli97 <yli4@oberlin.edu>
2021-03-23 18:11:15 +0800

    initial commet

--- pytorch test.py
+++ pytorch test.py
@@ -11,6 +11,8 @@ import numpy as np
 # Load model, use pretrained ResNet 50
 model = models.resnet50(pretrained=True)
 
+print(model)
+
 # Use CPU if no GPU available
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -24,58 +26,11 @@ for param in model.parameters():
 # nn.Sequential() apply new model/layer in sequence to previous model
 model.fc = nn.Sequential(nn.Linear(2048, 512), # Linear(input dim, hidden dim)
                                  nn.ReLU(), # Activation function
-                                 nn.Dropout(0.2),
+                                 nn.Dropout(0.2), #
                                  nn.Linear(512, 10), # Linear(hidden dim, output dim)
                                  nn.LogSoftmax(dim=1))
 criterion = nn.NLLLoss()
 optimizer = optim.Adam(model.fc.parameters(), lr=0.003)
 model.to(device)
 
-epochs = 1
-steps = 0
-running_loss = 0
-print_every = 10
-train_losses, test_losses = [], []
-for epoch in range(epochs):
-    for inputs, labels in trainloader:
-        steps += 1
-        inputs, labels = inputs.to(device), labels.to(device)
-        optimizer.zero_grad()
-        logps = model.forward(inputs)
-        loss = criterion(logps, labels)
-        loss.backward()
-        optimizer.step()
-        running_loss += loss.item()
-        
-        if steps % print_every == 0:
-            test_loss = 0
-            accuracy = 0
-            model.eval()
-            with torch.no_grad():
-                for inputs, labels in testloader:
-                    inputs, labels = inputs.to(device),
-                                      labels.to(device)
-                    logps = model.forward(inputs)
-                    batch_loss = criterion(logps, labels)
-                    test_loss += batch_loss.item()
-                    
-                    ps = torch.exp(logps)
-                    top_p, top_class = ps.topk(1, dim=1)
-                    equals = 
-                        top_class == labels.view(*top_class.shape)
-                    accuracy +=
-                   torch.mean(equals.type(torch.FloatTensor)).item()
-            train_losses.append(running_loss/len(trainloader))
-            test_losses.append(test_loss/len(testloader))                    
-            print(f"Epoch {epoch+1}/{epochs}.. "
-                  f"Train loss: {running_loss/print_every:.3f}.. "
-                  f"Test loss: {test_loss/len(testloader):.3f}.. "
-                  f"Test accuracy: {accuracy/len(testloader):.3f}")
-            running_loss = 0
-            model.train()
-torch.save(model, 'aerialmodel.pth')
-
-plt.plot(train_losses, label='Training loss')
-plt.plot(test_losses, label='Validation loss')
-plt.legend(frameon=False)
-plt.show()
+print("test")
